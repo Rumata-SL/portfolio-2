7d252a0cd3fd3ddeabee3cc96cec92b0df8a31a2
Rumata-SL <kirr7619@gmail.com>
2022-06-19 02:03:15 +0500

    ganged background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import './App.css';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Navigation, PATH} from "./components/navigation/Navigation";
@@ -6,17 +6,26 @@ import {Profile} from "./components/profile/Profile";
 import {Contacts} from "./components/contacts/Contacts";
 import {Skills} from "./components/skills/Skills";
 // @ts-ignore
+import lines from "./components/profile/video/Lines.mp4"
+// @ts-ignore
 // import videoBG from "./components/profile/video/2.mp4"
 // @ts-ignore
-import video from "./components/profile/video/nuva.mov"
-// @ts-ignore
+// import {video} from "./components/profile/video/nuva.mov"
 
 function App() {
+ // const [videoB, setVideoB] = useState(false)
+ //    useEffect(()=>{
+ //        setVideoB(true)
+ //    },[])
+
   return (
     <div className="App">
         <div className={"over"}></div>
-        <video src={video} autoPlay loop muted/>
+        <video src={lines} autoPlay loop muted/>
         {/*<video src={videoBG} autoPlay loop muted/>*/}
+        {/*<video src={video}  autoPlay loop muted/>*/}
+        {/*{videoB && <video src={video} autoPlay loop muted/>}*/}
+        {/*{videoB && <video src={videoBG} autoPlay loop muted/>}*/}
         <div className={"content"}>
             <div>
                 <Routes>
