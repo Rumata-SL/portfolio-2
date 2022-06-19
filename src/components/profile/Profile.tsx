d2f514f9340a96977cadfdece21516150ef708d4
Rumata-SL <kirr7619@gmail.com>
2022-06-20 02:15:00 +0500

    added react-typing-effect

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import {Links} from "../links/Links";
 import style from "./Profile.module.css"
+import ReactTypingEffect from "react-typing-effect";
 
 
 export const Profile = () => {
     return (
         <div className={style.wrapper}>
             <div className={style.container}>
-                <span>front-end developer</span>
-                <span>
-                    Hello, i`m Sergey
-                </span>
+                <ReactTypingEffect text={['<Front-end developer/>']}
+                                   typingDelay={5000}
+                                   speed={150}
+                                   eraseSpeed={60}
+                                   cursor={'|'}
+                                   className={style.dev}
+                />
+
             </div>
             <Links/>
         </div>
