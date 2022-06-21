ddf5f10f3858339fcc551eff8d9d0a6192819aa4
Rumata-SL <kirr7619@gmail.com>
2022-06-22 02:13:14 +0500

    added react-icons

--- src/components/links/Links.tsx
+++ src/components/links/Links.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import style from "./Links.module.css"
-import {NavLink} from "react-router-dom";
 import git from "./icons/github.png"
+import {FaGithubAlt, FaLinkedin, FaTelegramPlane} from "react-icons/fa";
+import {SiCodewars} from "@react-icons/all-files/si/SiCodewars";
 
 export const Links = () => {
     return (
         <div className={style.wrapper}>
+
             <div>
-                <a href={"https://github.com/Rumata-SL"}><img className={style.link} src={git} alt="github" width={30}/></a>
-            </div><div>
-                <a href={"https://github.com/Rumata-SL"}><img className={style.link} src={git} alt="github" width={30}/></a>
-            </div><div>
-                <a href={"https://github.com/Rumata-SL"}><img className={style.link} src={git} alt="github" width={30}/></a>
-            </div><div>
-                <a href={"https://github.com/Rumata-SL"}><img className={style.link} src={git} alt="github" width={30}/></a>
+                <a href={"https://github.com/Rumata-SL"} target="_blank"><FaGithubAlt className={style.link} /></a>
+            </div>
+            <div>
+                <a href={"https://github.com/Rumata-SL"} target="_blank"><FaLinkedin className={style.link}/></a>
+            </div>
+            <div>
+                <a href={"https://github.com/Rumata-SL"} target="_blank"><FaTelegramPlane className={style.link}/></a>
+            </div>
+            <div>
+                <a href={"https://github.com/Rumata-SL"} target="_blank"><SiCodewars className={style.link}/></a>
             </div>
 
 
