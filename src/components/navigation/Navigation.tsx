3adaab0634fc4deae56aee7699f583a3fd9bc8d3
Rumata-SL <kirr7619@gmail.com>
2022-06-22 02:36:47 +0500

    changed icons

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import style from "./Navigation.module.css"
+import {CgProfile} from "@react-icons/all-files/cg/CgProfile";
+import {GiSkills} from "@react-icons/all-files/gi/GiSkills";
+import {TiContacts} from "@react-icons/all-files/ti/TiContacts";
+import {AiOutlineProject} from "@react-icons/all-files/ai/AiOutlineProject";
 
 export enum PATH {
     PROFILE = "/profile/",
@@ -18,10 +22,10 @@ export const Navigation = () => {
     return (
         <div className={style.wrapper}>
             <div className={style.container}>
-                <NavLink className={setActive} to={PATH.PROFILE}></NavLink>
-                <NavLink className={setActive} to={PATH.SKILLS}></NavLink>
-                <NavLink className={setActive} to={PATH.WORKS}></NavLink>
-                <NavLink className={setActive} to={PATH.CONTACTS}></NavLink>
+                <NavLink className={setActive} to={PATH.PROFILE}><CgProfile className={style.icon}/></NavLink>
+                <NavLink className={setActive} to={PATH.SKILLS}><GiSkills className={style.icon}/></NavLink>
+                <NavLink className={setActive} to={PATH.WORKS}><AiOutlineProject className={style.icon}/></NavLink>
+                <NavLink className={setActive} to={PATH.CONTACTS}><TiContacts className={style.icon}/></NavLink>
 
             </div>
         </div>
