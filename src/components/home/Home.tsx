1b7509a03553369422217da7ed6023f3ed57ea61
Rumata-SL <kirr7619@gmail.com>
2022-07-17 02:27:50 +0500

    added library react-reveal

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React from "react"
 // import Fade from 'react-reveal/Fade'
-import ReactTypingEffect from 'react-typing-effect'
-
+import ReactTypingEffect from "react-typing-effect"
 import s from "./Home.module.css"
 
+const Fade = require("react-reveal/Fade")
 export const Home = () => {
 
     return (
-        <div className={s.mainBlock} id='main'>
+        <div className={s.mainBlock} id="main">
             <div className={s.container}>
-                {/*<Fade clear>*/}
-                    <div className={s.greeting}>
+                <div className={s.greeting}>
                 <span className={s.profession}>
-                   <ReactTypingEffect text={['<Front-end developer/>']}
+                   <ReactTypingEffect text={["<Front-end developer/>"]}
                                       speed={100}
                                       eraseSpeed={10}
-                                      cursor={'|'}
+                                      cursor={"|"}
                    />
                 </span>
-                        <h1>Hello, I'm <span className={s.name}>Sergey</span><br/>
-                            Welcome to my World.</h1>
-                    </div>
-                {/*</Fade>*/}
+                    <Fade left delay={"1000"} duration={"1000"}>
+                        <h1>Hello, I'm</h1>
+                    </Fade>
+                    <Fade left delay={"2000"} duration={"2000"}>
+                        <h1><span className={s.name}>Sergey</span></h1>
+                    </Fade>
+                    <Fade left delay={"3000"} duration={"3000"}>
+                        <h1>Welcome to my World.</h1>
+                    </Fade>
+                </div>
             </div>
         </div>
     )
 }
+
+/*
+<h1>Hello, I'm <span className={s.name}>Sergey</span><br/>
+    Welcome to my World.</h1>*/
