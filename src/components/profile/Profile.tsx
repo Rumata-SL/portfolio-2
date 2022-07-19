4861002bf8d5f7eab5f7b2edba0ad40a125f866b
Rumata-SL <kirr7619@gmail.com>
2022-07-19 23:27:41 +0500

    changed Profile.tsx

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {Links} from "../links/Links";
 import style from "./Profile.module.css"
-import photo from "../../assets/img/me.png"
+// import photo from "../../assets/img/me.png"
+import photo from "../../assets/img/fhotome.png"
 
 const Zoom = require("react-reveal/Zoom")
 
@@ -10,21 +11,21 @@ export const Profile = () => {
     return (
         <div className={style.wrapper}>
             <div className={style.container}>
-                <Zoom  duration={"2000"}>
-                    <div className={style.containerTitle}>
-                        <span className={style.title}>About me</span>
-                        <span className={style.text}>Привет друзья! Позвольте не много рассказать о себе. У меня высшее образование в области транспортных систем, и много лет я работал в этой сфере. Но несколько лет назад я открыл для себя мир web разработки, и решил связать связать свою жизнь с программированием. В настоящее время я разрабатываю сайты на React в связке с TypeScript.
-
-                    </span>
-                    </div>
-                </Zoom>
-
-                <Zoom delay={"1000"} duration={"2000"}>
-                    <div className={style.mePhoto}>
+                <div className={style.mePhoto}>
+                    <Zoom delay={"1000"} duration={"2000"}>
                         <img src={photo} alt="photoMe"
                              className={style.imgPhoto}/>
-                    </div>
+                    </Zoom>
+                </div>
+                    <div className={style.containerTitle}>
+                <Zoom  duration={"2000"}>
+                        <span className={style.title}>About me</span>
+                        <span className={style.text}>Привет друзья! Позвольте не много рассказать о себе. У меня высшее образование в области транспортных систем, и много лет я работал в этой сфере. Но несколько лет назад я открыл для себя мир web разработки, и решил связать связать свою жизнь с программированием. В настоящее время я разрабатываю сайты на React в связке с TypeScript.
+                    </span>
                 </Zoom>
+                    </div>
+
+
             </div>
 
             <Links/>
